68be889ff844033515f05b7f34970e9ed7990fb5
Darya <{darozina@gmail.com}>
2021-09-20 14:03:08 +0300

    задание к уроку 7

---
+++ lesson7_hw3.py
@@ -0,0 +1,48 @@
+"""Реализовать программу работы с органическими клетками. Необходимо создать класс Клетка.
+В его конструкторе инициализировать параметр, соответствующий количеству клеток (целое число).
+В классе должны быть реализованы методы перегрузки арифметических операторов:
+сложение (__add__()), вычитание (__sub__()), умножение (__mul__()), деление (__truediv__()).
+Данные методы должны применяться только к клеткам и выполнять увеличение, уменьшение, умножение и обычное (не целочисленное) деление клеток,
+соответственно. В методе деления должно осуществляться округление значения до целого числа.
+
+В классе необходимо реализовать метод make_order(), принимающий экземпляр класса и количество ячеек в ряду.
+Данный метод позволяет организовать ячейки по рядам.
+Метод должен возвращать строку вида *****\n*****\n*****..., где количество ячеек между \n равно переданному аргументу.
+Если ячеек на формирование ряда не хватает, то в последний ряд записываются все оставшиеся.
+Например, количество ячеек клетки равняется 12, количество ячеек в ряду — 5. Тогда метод make_order() вернет строку: *****\n*****\n**.
+Или, количество ячеек клетки равняется 15, количество ячеек в ряду — 5. Тогда метод make_order() вернет строку: *****\n*****\n*****."""
+
+class Cell:
+    def __init__(self, quantity):
+        self.quantity = int(quantity)
+
+    def __add__(self, other):
+        return f'Increase of cells: {self.quantity + other.quantity}'
+
+    def __sub__(self, other):
+        sub = self.quantity - other.quantity
+        return f'Decrease of cells: {sub} ' if sub > 0 else 'Cells disappeared'
+
+    def __mul__(self, other):
+        return f'Multiplication of cells: {self.quantity * other.quantity}'
+
+    def __truediv__(self, other):
+        return f'Division of cells: {self.quantity // other.quantity}'
+
+
+    def make_order(self, row):
+        result = ''
+        for i in range(int(self.quantity / row)):
+            result += '*' * row + '\n'
+        result += '*' * (self.quantity % row) + '\n'
+        return result
+
+
+cell = Cell(15)
+cell_2 = Cell(10)
+
+print(cell + cell_2)
+print(cell - cell_2)
+print(cell / cell_2)
+print(cell * cell_2)
+print(cell.make_order(5))
